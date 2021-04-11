c292c32d6f3936dc1ff7601cd3afdd691d27c0c8
walterego <walter.scimone@gmail.com>
2021-04-11 06:12:40 -0400

    function to dinamically create README files implemented

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# readme-generator
+
+        <h1>sdsd</h1>
+          
+        ## Purpose
+        sdsdd
+        
+        ## Languages Used
+        sdsd
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
 // TODO: Create an array of questions for user input
 
 const questions = () => { 
-    inquirer.prompt([
+    return inquirer.prompt([
         {
             type: "input",
             name: "title",
@@ -25,13 +26,28 @@ const questions = () => {
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-let init = () => {
+let init = async () => {
     try {
-        const answers = questions();
+        const answers = await questions();
         console.log(answers);
+        function createReadme(answers) {
+            return `
+        <h1>${answers.title}</h1>
+          
+        ## Purpose
+        ${answers.purpose}
+        
+        ## Languages Used
+        ${answers.languages}`
+        
+        
+        };
+        let createContent = createReadme(answers);
+        util.promisify(fs.writeFile)('README.md', createContent.toString());
     }   catch(err) {
         console.log(err);
     }
