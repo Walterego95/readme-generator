ab6f6c4df3808837198555d3101d7666f40f84d4
walterego <walter.scimone@gmail.com>
2021-04-11 03:37:50 -0400

    created function to initialize input...

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-//const questions = [];
 
-let questions = () => {
-    return inquirer.prompt([
+const questions = () => { 
+    inquirer.prompt([
         {
             type: "input",
             name: "title",
@@ -23,15 +22,20 @@ let questions = () => {
         },
     ])
 };
-console.log(inquirer.prompt);
-
 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+let init = () => {
+    try {
+        const answers = questions();
+        console.log(answers);
+    }   catch(err) {
+        console.log(err);
+    }
+  }
 
 // Function call to initialize app
 init();
