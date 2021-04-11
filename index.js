d1b5918d0744505bd56801df75babff85487f547
walterego <walter.scimone@gmail.com>
2021-04-11 03:09:51 -0400

    packages added for application, question function for input in progress...

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+let questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the project?",
+        },
+        {
+            type: "input",
+            name: "purpose",
+            message: "Define in a few words the purpose:"
+        },
+        {
+            type: "input",
+            name: "languages",
+            message: "What programming languages did you use?",
+        },
+    ])
+};
+console.log(inquirer.prompt);
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
